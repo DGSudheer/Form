0f99444ae933957e9c05cdd203eb2a7c77059a3d
D G Sudheer <dgsudheer@gmail.com>
2025-07-06 18:49:56 +0530

    basic form

--- src/CommentForm.jsx
+++ src/CommentForm.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 export default function CommentForm() {
     return(
         <div>
-
+            <input type="text" placeholder="enter the full name"/>
+            <button>Submit</button>
         </div>
     );
 }
